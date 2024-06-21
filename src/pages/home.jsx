1e0e9f16fcff85be547d5c573be846c2c1082ad2
mitrons141 <mitrajeetdas01@gmail.com>
2024-06-21 21:35:32 +0530

    margin changes

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -92,7 +92,7 @@ const Home = () => {
         </Link>
       </div>
       <div className="container mx-auto p-4">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-40">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-96 md:mt-40">
           {error ? (
             <h1 className="text-center text-gray-500 mt-40">{error}</h1>
           ) : (
